56a317c2546d25b4756ab8c5bef134267934013d
Gregory <32060161+ggmaleva@users.noreply.github.com>
2023-12-08 16:32:55 +0300

    Refactoring (#14)
    * fix workflow
    
    * refactoring
    
    * fix name
    
    ---------
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- proto/service.thrift
+++ proto/service.thrift
@@ -4,18 +4,23 @@ namespace erlang exrates.service
 include "base.thrift"
 include "events.thrift"
 
+struct CurrencyData {
+    1: required base.CurrencySymbolicCode source_currency
+    2: required base.CurrencySymbolicCode destination_currency
+}
+
 struct GetCurrencyExchangeRateResult {
-    1: required base.CurrencyData currency_data
+    1: required CurrencyData currency_data
     2: required base.Rational exchange_rate
     3: required base.Timestamp timestamp
 }
 
 struct GetCurrencyExchangeRateRequest {
-    1: required base.CurrencyData currency_data
+    1: required CurrencyData currency_data
 }
 
 service ExchangeRateService {
 
-    CurrencyExchangeRateResult GetExchangeRateData (1: GetCurrencyExchangeRateRequest request)
+    GetCurrencyExchangeRateResult GetExchangeRateData (1: GetCurrencyExchangeRateRequest request)
 
 }
