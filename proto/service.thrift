9794d1405e49d78ef8514dad04ea3989713aa184
Gregory <32060161+ggmaleva@users.noreply.github.com>
2024-05-30 13:08:38 +0300

    add GetConvertedAmount method (#18)
    * add GetConvertedAmount method
    
    * add comment
    
    ---------
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- proto/service.thrift
+++ proto/service.thrift
@@ -2,7 +2,9 @@ namespace java dev.vality.exrates.service
 namespace erlang exrates.service
 
 include "base.thrift"
-include "events.thrift"
+
+/** Идентификатор, указывающий на источник получения курсов валют */
+typedef string SourceID
 
 struct CurrencyData {
     1: required base.CurrencySymbolicCode source_currency
@@ -19,11 +21,32 @@ struct GetCurrencyExchangeRateRequest {
     1: required CurrencyData currency_data
 }
 
+/**
+* Запрос на конвертацию, где:
+* source - валюта, из которой конвертируют
+* destination - валюта, в которую конвертируют
+* amount - сумма в валюте source в минорных денежных единицах
+* datetime - дата и время, за которое нужно провести конвертацию
+*/
+struct ConversionRequest {
+    1: required base.CurrencySymbolicCode source
+    2: required base.CurrencySymbolicCode destination
+    3: required base.Amount amount
+    4: optional base.Timestamp datetime
+}
+
 exception ExRateNotFound {}
+exception CurrencyNotFound {}
 
 service ExchangeRateService {
 
     GetCurrencyExchangeRateResult GetExchangeRateData (1: GetCurrencyExchangeRateRequest request)
      throws (1: ExRateNotFound ex1)
+
+     base.Rational GetConvertedAmount(1: SourceID source, 2: ConversionRequest request)
+             throws (
+                 1: ExRateNotFound ex1
+                 2: CurrencyNotFound ex2
+             )
 
 }
